c9ad335b03a1c7c0a118fb23b93f3287e840810b
AdamBGZ <adamcodm2004@gmail.com>
2026-05-08 15:05:52 +0100

    Mis à jour des test +fix problemes apres merge

--- service-gestion-incidents/src/main/java/com/sgitu/servicegestionincidents/repository/IncidentRepository.java
+++ service-gestion-incidents/src/main/java/com/sgitu/servicegestionincidents/repository/IncidentRepository.java
@@ -5,6 +5,7 @@ import com.sgitu.servicegestionincidents.model.enums.NiveauGravite;
 import com.sgitu.servicegestionincidents.model.enums.StatutIncident;
 import com.sgitu.servicegestionincidents.model.enums.TypeIncident;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
